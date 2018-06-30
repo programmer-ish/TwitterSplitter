b0a3108b1cfd155f08e8619e980bf8a233869d71
ishita <ishita.srivastava@st.niituniversity.in>
2018-06-30 23:48:32 +0800

    Updated splitMessage to split tweets if more than 50 characters

--- public/script.js
+++ public/script.js
@@ -1,14 +1,28 @@
 function postMessage() {
     var tweet = document.getElementById("tweet").value;
+    tweet=splitMessage(tweet);
     var postingWall = document.getElementById("list"); //get the list from html
     var new_post = document.createElement("li_a");
     var new_tweet = document.createTextNode(tweet);
-    splitMessage(new_tweet);
     new_post.appendChild(new_tweet); //put value in element
     postingWall.appendChild(new_post); //put element in the old list
 
 }
 
-function splitMessage(){
-    
+function splitMessage(tweet) {
+    console.log("new tweet"+tweet.length);
+    var newTweets= [];
+    if(tweet<=50)
+        return tweet;
+    else{
+        tweet=tweet.toString();
+        newTweets=tweet.match(/.{1,46}/g); // not just {46} so that we include strings that are not multiples of 46
+        var noOfTweets= newTweets.length;
+        for(i=0,j=0;i<noOfTweets;i++){
+            j=i+1;
+            newTweets[i]=j+"/"+noOfTweets+" "+newTweets[i];
+        }
+        console.log(newTweets);
+        return newTweets;
+    }
 }
