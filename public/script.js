091e318707148d8f598d83d3f9961ce283e518fc
ishita <ishita.srivastava@st.niituniversity.in>
2018-07-02 01:09:15 +0800

    Updated splitMessage so messages will only be split on whitespace

--- public/script.js
+++ public/script.js
@@ -1,28 +1,49 @@
 function postMessage() {
-    var tweet = document.getElementById("tweet").value;
-    tweet=splitMessage(tweet);
-    var postingWall = document.getElementById("list"); //get the list from html
-    var new_post = document.createElement("li_a");
-    var new_tweet = document.createTextNode(tweet);
-    new_post.appendChild(new_tweet); //put value in element
-    postingWall.appendChild(new_post); //put element in the old list
+    try {
+        var postingWall = document.getElementById("list"); //get the list from html
+        var tweet = document.getElementById("tweet").value;
+        tweet = splitMessage(tweet);
+        if (tweet !== undefined) {
+            for (i = 0; i < tweet.length; i++) {
+                var new_post = document.createElement("li");
+                var new_tweet = document.createTextNode(tweet[i]);
+                new_post.appendChild(new_tweet); //put value in element
+                postingWall.appendChild(new_post); //put element in the old list
+            }
+        }
+    }
+    catch{
+        console.log("Error encountered in postMessage");
+    }
 
 }
 
 function splitMessage(tweet) {
-    console.log("new tweet"+tweet.length);
-    var newTweets= [];
-    if(tweet<=50)
-        return tweet;
-    else{
-        tweet=tweet.toString();
-        newTweets=tweet.match(/.{1,46}/g); // not just {46} so that we include strings that are not multiples of 46
-        var noOfTweets= newTweets.length;
-        for(i=0,j=0;i<noOfTweets;i++){
-            j=i+1;
-            newTweets[i]=j+"/"+noOfTweets+" "+newTweets[i];
+    try {
+        if (tweet.length <= 50)
+            return tweet;
+        else {
+            tweet = tweet.toString();
+            var newTweets = [];
+            for (i = 0, startOfPost = 0, endOfPost = 46; endOfPost < tweet.length; startOfPost = endOfPost + 1, endOfPost += 46) {
+                endOfPost = tweet.lastIndexOf(" ", endOfPost);
+                if (endOfPost === -1) {
+                    alert(" message contains a span of non-whitespace characters longer than 46 characters");
+                    return;
+                }
+                newTweets[i] = tweet.substring(startOfPost, endOfPost);
+                i++;
+            }
+            newTweets[i] = tweet.substring(startOfPost, tweet.length);
+            var noOfTweets = newTweets.length;
+            for (i = 0, j = 0; i < noOfTweets; i++) {
+                j = i + 1;
+                newTweets[i] = j + "/" + noOfTweets + " " + newTweets[i];
+            }
+            return newTweets;
         }
-        console.log(newTweets);
-        return newTweets;
+    }
+    catch{
+        console.log("Error encountered in splitMessage");
     }
 }
